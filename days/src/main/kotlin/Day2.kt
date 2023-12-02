179176b753fe38b367ec5c0e348bea9d1d276d2a
apinx <me@apinx.dk>
2023-12-02 10:31:31 +0100

    Day 2

--- days/src/main/kotlin/Day2.kt
+++ days/src/main/kotlin/Day2.kt
@@ -1,9 +1,48 @@
 package day2
 
-fun task1(input: List<String>): String {
-    return ""
+fun task1(input: List<String>, blue: Int, red: Int, green: Int): Int {
+    return input.map { parse(it) }.sumOf { g ->
+        if (g.sets.any { c ->
+                c.red > red || c.blue > blue || c.green > green
+            }) {
+            0
+        } else {
+            g.gameId
+        }
+    }
 }
 
-fun task2(input: List<String>): String {
-    return ""
+fun task2(input: List<String>): Int {
+    return input.map { parse(it) }.sumOf { g ->
+        g.sets.maxOf { c -> c.red } * g.sets.maxOf { c -> c.green } * g.sets.maxOf { c -> c.blue }
+    }
 }
+
+private fun parse(s: String): Game {
+    val spl = s.split(":")
+    val gameId = Regex("Game (\\d+)").find(spl[0])!!.groups[1]!!.value.toInt()
+    val setsSpl = spl[1].trim().split(";")
+
+    val regex = Regex("((\\d+) ([a-z]+)[, ]*)")
+    val cubes = setsSpl.map { l ->
+        var green = 0
+        var red = 0
+        var blue = 0
+        for (hit in regex.findAll(l)) {
+            val toInt = hit.groups[2]!!.value.toInt()
+            when (hit.groups[3]!!.value) {
+                "green" -> green += toInt
+                "red" -> red += toInt
+                "blue" -> blue += toInt
+            }
+        }
+
+        Cube(blue, red, green)
+    }.toList()
+
+    return Game(gameId, cubes)
+}
+
+private data class Game(val gameId: Int, val sets: List<Cube>)
+
+private data class Cube(val blue: Int, val red: Int, val green: Int)
--- days/src/test/kotlin/Day2Test.kt
+++ days/src/test/kotlin/Day2Test.kt
@@ -8,14 +8,23 @@ internal class Day2Test {
 
     @Test
     fun testTaskOne() {
-        assertEquals("", task1(listOf()))
-        assertEquals("", task1(readThisPuzzle()))
+        assertEquals(8, task1("""Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".lines(), 14, 12, 13))
+        assertEquals(1931, task1(readThisPuzzle(), 14, 12, 13))
     }
 
     @Test
     fun testTaskTwo() {
-        assertEquals("", task2(listOf()))
-        assertEquals("", task2(readThisPuzzle()))
+        assertEquals(2286, task2("""Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".lines()))
+        assertEquals(83105, task2(readThisPuzzle()))
+
     }
 
     private fun readThisPuzzle(): List<String> {
