2ed1fffae86ab3882b4bdcf255e476bf61088892
Morten Søgård Jensen <morten.sogard.jensen@systematic.com>
2023-12-04 09:06:24 +0100

    Day 4

--- days/src/main/kotlin/Day4.kt
+++ days/src/main/kotlin/Day4.kt
@@ -1,9 +1,49 @@
 package day4
 
 fun task1(input: List<String>): String {
-    return ""
+    return input.sumOf { l ->
+        parse(l).multWinningNumbers()
+    }.toString()
 }
 
 fun task2(input: List<String>): String {
-    return ""
+    val cards = input.map { parse(it) }
+    val amt = IntArray(cards.size) { 1 }
+
+    cards.withIndex().forEach {
+        val pointsForCard = it.value.winningNumbers()
+        val range = it.index + 1..it.index + pointsForCard
+        for (i in range) {
+            amt[i] += amt[it.index]
+        }
+    }
+
+    return amt.sum().toString()
+}
+
+
+private fun parse(l: String): Card {
+    val spl = l.split(":")
+    val (winStr, numStr) = spl[1].split("|")
+    val win = winStr.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+    val num = numStr.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+    return Card(win, num)
+}
+
+private data class Card(val win: List<Int>, val num: List<Int>) {
+    fun multWinningNumbers(): Int {
+        var sum = 0
+        for (n in num.filter { win.contains(it) }) {
+            if (sum == 0) {
+                sum = 1
+            } else {
+                sum *= 2
+            }
+        }
+        return sum
+    }
+
+    fun winningNumbers(): Int {
+        return num.count { win.contains(it) }
+    }
 }
--- days/src/test/kotlin/Day4Test.kt
+++ days/src/test/kotlin/Day4Test.kt
@@ -8,14 +8,24 @@ internal class Day4Test {
 
     @Test
     fun testTaskOne() {
-        assertEquals("", task1(listOf()))
-        assertEquals("", task1(readThisPuzzle()))
+        assertEquals("13", task1("""Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""".lines()))
+        assertEquals("23028", task1(readThisPuzzle()))
     }
 
     @Test
     fun testTaskTwo() {
-        assertEquals("", task2(listOf()))
-        assertEquals("", task2(readThisPuzzle()))
+        assertEquals("30", task2("""Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""".lines()))
+        assertEquals("9236992", task2(readThisPuzzle()))
     }
 
     private fun readThisPuzzle(): List<String> {
