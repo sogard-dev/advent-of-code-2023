c6b227b76763e9f3922aa40598c2a759ac13a589
Morten Søgård Jensen <morten.sogard.jensen@systematic.com>
2023-12-06 09:14:12 +0100

    Day 6

--- days/src/main/kotlin/Day6.kt
+++ days/src/main/kotlin/Day6.kt
@@ -1,9 +1,26 @@
 package day6
 
 fun task1(input: List<String>): String {
-    return ""
+    val times = parseNumbers(input[0])
+    val distances = parseNumbers(input[1])
+    return times.indices.map { howManyCanBeat(times[it], distances[it]) }.fold(1) { acc, v -> acc * v }.toString()
 }
 
 fun task2(input: List<String>): String {
-    return ""
+    val times = parseNumbers(input[0].replace(" ", ""))
+    val distances = parseNumbers(input[1].replace(" ", ""))
+    return times.indices.sumOf { howManyCanBeat(times[it], distances[it]) }.toString()
+}
+
+private fun howManyCanBeat(allowedTime: Long, distance: Long): Int {
+    return (0..allowedTime).count { isFaster(it, allowedTime, distance) }
+}
+
+fun isFaster(pressTime: Long, allowedTime: Long, recordDistance: Long): Boolean {
+    return pressTime * (allowedTime - pressTime) > recordDistance
+}
+
+fun parseNumbers(s: String): List<Long> {
+    val regex = Regex("(\\d+)")
+    return regex.findAll(s).map { g -> g.groups[1]!!.value.toLong() }.toList()
 }
--- days/src/test/kotlin/Day6Test.kt
+++ days/src/test/kotlin/Day6Test.kt
@@ -6,16 +6,19 @@ import kotlin.test.Test
 
 internal class Day6Test {
 
+    private val testInput = """Time:      7  15   30
+    Distance:  9  40  200""".lines()
+
     @Test
     fun testTaskOne() {
-        assertEquals("", task1(listOf()))
-        assertEquals("", task1(readThisPuzzle()))
+        assertEquals("288", task1(testInput))
+        assertEquals("4811940", task1(readThisPuzzle()))
     }
 
     @Test
     fun testTaskTwo() {
-        assertEquals("", task2(listOf()))
-        assertEquals("", task2(readThisPuzzle()))
+        assertEquals("71503", task2(testInput))
+        assertEquals("30077773", task2(readThisPuzzle()))
     }
 
     private fun readThisPuzzle(): List<String> {
