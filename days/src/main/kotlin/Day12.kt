8122cc919af51a70da43140815f131d1539e9de7
Morten Søgård Jensen <morten.sogard.jensen@systematic.com>
2023-12-12 18:24:02 +0100

    Day 12

--- days/src/main/kotlin/Day12.kt
+++ days/src/main/kotlin/Day12.kt
@@ -1,10 +1,95 @@
 package day12
 
+import util.getNumbers
+
 fun task1(input: List<String>): String {
-    return ""
+    return input
+            .map { solve(it) }
+            .sum()
+            .toString()
 }
 
-
 fun task2(input: List<String>): String {
-    return ""
+    return input
+            .map { expand(it) }
+            .map { solve(it) }
+            .sum()
+            .toString()
+}
+
+fun expand(s: String): String {
+    val input = s.split(" ")[0]
+    val numbers = getNumbers(s.split(" ")[1]).joinToString(",")
+
+    return "$input?$input?$input?$input?$input $numbers,$numbers,$numbers,$numbers,$numbers"
+}
+
+private fun solve(str: String): Long {
+    val text = ".${str.split(" ")[0]}."
+    val numbers = getNumbers(str.split(" ")[1])
+    val cache: MutableMap<Pair<Int, Int>, Long> = mutableMapOf()
+
+    fun recurse(changeAt: Int, nextNumberIndex: Int): Long {
+        if (nextNumberIndex == numbers.size) {
+            if (text.substring(changeAt).contains('#')) {
+                return 0
+            }
+
+            return 1
+        }
+
+        val cacheKey = Pair(changeAt, nextNumberIndex)
+        val cacheHit = cache[cacheKey]
+        if (cacheHit != null) {
+            return cacheHit
+        }
+
+        val nextToFit = numbers[nextNumberIndex]
+        var mustFit = false
+        var optionLength = 0
+        var options = 0L
+
+        for (i in changeAt until text.length) {
+            val c = text[i]
+            if (!mustFit && c == '#') { //Must fit here
+                mustFit = true
+            }
+            if (mustFit) {
+                if (c == '.') {
+                    options = 0
+                    break
+                } else {
+                    optionLength++
+                }
+
+                if (optionLength == nextToFit) {
+                    if (text[i + 1] == '#') {
+                        options = 0
+                        break
+                    }
+
+                    options += recurse(i + 2, nextNumberIndex + 1)
+                    break
+                }
+            } else if (c == '?') {
+                if (i + nextToFit >= text.length) {
+                    options = 0
+                    break
+                }
+
+                val canFitHere = text.subSequence(i until i + nextToFit).all { it == '#' || it == '?' }
+                if (canFitHere && text[i + nextToFit] != '#') {
+                    options += recurse(i + nextToFit + 1, nextNumberIndex + 1)
+                }
+
+                options += recurse(i + 1, nextNumberIndex)
+                break
+            }
+        }
+
+        cache[cacheKey] = options
+        return options
+    }
+
+    return recurse(text.indexOfFirst { it == '?' || it == '#' }, 0)
 }
--- days/src/test/kotlin/Day12Test.kt
+++ days/src/test/kotlin/Day12Test.kt
@@ -6,17 +6,36 @@ import kotlin.test.Test
 
 
 internal class Day12Test {
-    
+
+    private val testInput = """???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1""".lines()
+
     @Test
-    fun testTaskOne() {
-        assertEquals("", task1(listOf()))
-        assertEquals("", task1(readThisPuzzle()))
+    fun tests() {
+        assertEquals("4", task1("????.######..#####. 1,6,5".lines()))
+        assertEquals("3", task1(".#???#.#??.#???. 5,1,1,1".lines()))
+        assertEquals("1", task1("???.### 1,1,3".lines()))
+        assertEquals("4", task1(".??..??...?##. 1,1,3".lines()))
+        assertEquals("1", task1("?#?#?#?#?#?#?#? 1,3,1,6".lines()))
+        assertEquals("1", task1("????.#...#... 4,1,1".lines()))
+        assertEquals("10", task1("?###???????? 3,2,1".lines()))
+
+        assertEquals("21", task1(testInput))
+        assertEquals("525152", task2(testInput))
+    }
+
+    @Test
+    fun taskOne() {
+        assertEquals("7251", task1(readThisPuzzle()))
     }
 
     @Test
     fun testTaskTwo() {
-        assertEquals("", task2(listOf()))
-        assertEquals("", task2(readThisPuzzle()))
+        assertEquals("2128386729962", task2(readThisPuzzle()))
     }
 
     private fun readThisPuzzle(): List<String> {
