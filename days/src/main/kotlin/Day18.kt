0c6d7443c232ee68af75b4c976dc9776110526c9
Morten Søgård Jensen <morten.sogard.jensen@systematic.com>
2023-12-18 15:20:40 +0100

    Day 18

--- days/src/main/kotlin/Day18.kt
+++ days/src/main/kotlin/Day18.kt
@@ -1,10 +1,68 @@
 package day18
 
-fun task1(input: List<String>): String {
-    return ""
-}
+import kotlin.math.absoluteValue
 
+fun task1(input: List<String>): String {
+    return solve(input)
+}
 
 fun task2(input: List<String>): String {
-    return ""
+    val regex = Regex("(\\w+)")
+    return solve(input.map {
+        val (_, _, hex) = regex.findAll(it).map { p -> p.groups[0]!!.value }.toList()
+        val steps = hex.substring(0..4).toLong(16)
+        val dir = when (hex[5]) {
+            '0' -> "R"
+            '1' -> "D"
+            '2' -> "L"
+            '3' -> "U"
+            else -> throw RuntimeException()
+        }
+
+        "$dir $steps (#0dc571)"
+    })
 }
+
+private fun solve(input: List<String>): String {
+    val regex = Regex("(\\w+)")
+
+    var lastPosition = 0L to 0L
+
+    val positions = input.map {
+        val (dir, bStr, _) = regex.findAll(it).map { p -> p.groups[0]!!.value }.toList()
+        val len = bStr.toLong()
+
+        val newPos = when (dir) {
+            "R" -> lastPosition.first to lastPosition.second + len
+            "L" -> lastPosition.first to lastPosition.second - len
+            "U" -> lastPosition.first - len to lastPosition.second
+            "D" -> lastPosition.first + len to lastPosition.second
+            else -> throw RuntimeException()
+        }
+
+        val prev = lastPosition
+        lastPosition = newPos
+        prev to lastPosition
+    }
+
+
+    return calc(positions).toString()
+}
+
+private fun calc(edges: List<Pair<Pair<Long, Long>, Pair<Long, Long>>>): Long {
+    //https://en.wikipedia.org/wiki/Pick%27s_theorem
+    //https://en.wikipedia.org/wiki/Shoelace_formula
+    val area = edges.sumOf { (c, n) -> c.first * n.second - c.second * n.first }.absoluteValue / 2
+    val boundary = edges.sumOf { e -> (e.second - e.first).manhatten() }
+
+    return area + boundary / 2 + 1
+}
+
+private fun Pair<Long, Long>.manhatten(): Long {
+    return first.absoluteValue + second.absoluteValue
+}
+
+private operator fun Pair<Long, Long>.minus(o: Pair<Long, Long>): Pair<Long, Long> {
+    return o.first - this.first to o.second - this.second
+}
+
--- days/src/test/kotlin/Day18Test.kt
+++ days/src/test/kotlin/Day18Test.kt
@@ -6,17 +6,32 @@ import kotlin.test.Test
 
 
 internal class Day18Test {
-    
+
+    private val testInput = """R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)""".lines()
+
     @Test
     fun testTaskOne() {
-        assertEquals("", task1(listOf()))
-        assertEquals("", task1(readThisPuzzle()))
+        assertEquals("62", task1(testInput))
+        assertEquals("48652", task1(readThisPuzzle()))
     }
 
     @Test
     fun testTaskTwo() {
-        assertEquals("", task2(listOf()))
-        assertEquals("", task2(readThisPuzzle()))
+        assertEquals("952408144115", task2(testInput))
+        assertEquals("45757884535661", task2(readThisPuzzle()))
     }
 
     private fun readThisPuzzle(): List<String> {
